8536fa85adc6261d7a9dfc2f16f32b9e7b2d2714
priyesshrai <priyeshrai369@gmail.com>
2025-04-10 15:10:12 +0530

    Update SideBar component to modify call-to-action button and enhance styling in globals.css

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -44,13 +44,14 @@ export default function SideBar() {
           <p dangerouslySetInnerHTML={{ __html: getTrimmedBio() }} />
 
           <div className="cta-button-container">
-            <Link href='tel:+919554333327'>
+            {/* <Link href='tel:+919554333327'>
               <i className="hgi hgi-stroke hgi-call-ringing-02"></i>
               <span>Book A Call</span>
             </Link>
             <button onClick={handleEmailBtn}>
               <i className="hgi hgi-stroke hgi-copy-01"></i> Copy Email
-            </button>
+            </button> */}
+            <button>Ping to Show Interest<i className="hgi hgi-stroke hgi-touch-09"></i> </button>
           </div>
 
           <div className="social-media-container">
